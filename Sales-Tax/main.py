0628a1734da1b7570d6b022b45574d35d81b5a82
HaydinD <haydindavis@crb1.net>
2024-09-05 10:38:29 -0600

    Update main.py

--- Sales-Tax/main.py
+++ Sales-Tax/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 9/5/24
 # Sales Tax Programming Project
 # COSC 2409 DNT
 #
@@ -10,13 +10,18 @@
 # Constants for the state and county tax rates
 
 # Get the amount of the purchase.
-
+purchase=float(input('Enter Project Sales: '))
 # Calculate the state sales tax.
-
+tax1=purchase*0.05
 # Calculate the county sales tax.
-
+tax2=purchase*0.025
 # Calculate the total tax.
-
+tax=tax1+tax2
 # Calculate the total of the sale.
-
+total=tax+purchase
 # Print information about the sale.
+print('The purchase is $', format(purchase,',.2f'))
+print('The state tax is $', format(tax1,',.2f'))
+print('The county tax is $', format(tax2,',.2f'))
+print('The total tax is $', format(tax,',.2f'))
+print('The total is $', format(total,',.2f'))
