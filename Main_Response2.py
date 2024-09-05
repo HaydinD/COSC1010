779b49f5b874eaac5b91aea46c21508b05572f2c
HaydinD <haydindavis@crb1.net>
2024-09-05 10:11:57 -0600

    Add files via upload

---
+++ Main_Response2.py
@@ -0,0 +1,15 @@
+#
+# Haydin Davis
+# 9/5/24
+# Sales Prediction Programming Project
+# COSC 2409 DNT
+#
+
+# Variables to hold the sales total and the profit
+
+# Get the amount of projected sales.
+total_sales=float(input('Enter Project Sales: '))
+# Calculate the projected profit.
+profit=total_sales*0.23
+# Print the projected profit.
+print('The profit is $', format(profit,',.2f'))
