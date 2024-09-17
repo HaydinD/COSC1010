d68a30392c812dd255a00959212b3102021645ce
HaydinD <haydindavis@crb1.net>
2024-09-17 10:20:53 -0600

    Update main.py

--- Hot-Dog-Cookout-Calculator/main.py
+++ Hot-Dog-Cookout-Calculator/main.py
@@ -1,7 +1,25 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 9/16
 # Hot Dog Cookout Calculator Programming Project
 # COSC 2409 DNT
-#
+#Ask for the amount of people
+people=int(input('How many people are going to be at the cookout?(whole number): '))
+#Divide the people by the amount in packs
+minHotDogs=int(people/10)
+minBuns=int(people/8)
+leftoverDogs=people%10
+leftoverBuns=people%8
 # Use comments liberally throughout the program. 
+if leftoverDogs==0:
+    print('You will need ', minHotDogs,'packs of hot dogs.')
+else:
+    print('You will need ', minHotDogs+1,'packs of hot dogs.')
+
+if leftoverBuns==0:
+    print('You will need ', minBuns,'packs of buns.')
+else:
+    print('You will need ', minBuns+1,'packs of buns.')
+
+print('You will have ', leftoverDogs,'hot dogs left over.')
+print('You will have ', leftoverBuns,'buns left over.')
