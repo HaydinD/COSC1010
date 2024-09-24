1d0257e8a9d4c68e0a33ebb3497bc8d859694c09
HaydinD <haydindavis@crb1.net>
2024-09-24 10:16:45 -0600

    Update kilometer_converter.py

--- Kilometer-Converter/kilometer_converter.py
+++ Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 9/24
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
-#
+CONVERSION_FACTOR=0.6214
 # Use comments liberally throughout the program. 
+def main():
+    kilometers=float(input('Enter a distance in kilometers: '))
+    miles(kilometers)
+def miles(km):
+    miles=km*CONVERSION_FACTOR
+
+    print(km,'kilometers equals',miles,'miles.')
+
+main()
