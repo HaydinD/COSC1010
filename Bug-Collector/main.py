79bba1339b0c0040fab6b82f0b835cbe22e93c86
HaydinD <haydindavis@crb1.net>
2024-09-24 09:37:09 -0600

    Update main.py

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,11 +1,15 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 9/24
 # Bug Collector Programming Project
 # COSC 2409 DNT
 #
 # Initialize variables for bugs and total number of bugs collected.
-
+total=0
 # Get number of bugs collected each day using a for loop.
-
+for day in range(1,8):
+    print('Enter the bugs collected on day',day)
+    bugs=int(input())
+    total+=bugs
 # Display the total number of bugs collected.
+print('you collected a total of ',total,'bugs')
