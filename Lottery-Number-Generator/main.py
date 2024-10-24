3fbc65f148fd7f611ae50179ac64d22ba38575a1
HaydinD <haydindavis@crb1.net>
2024-10-24 09:59:00 -0600

    Update main.py

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,22 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 10/24
 # Lottery Number Generator Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+MAX=7
+START=0
+END=9
+
+def main():
+    numbers=[0]*7
+
+    for index in range(MAX):
+        numbers[index]= random.randint(START,END)
+
+    print("Here are your lottery numbers:")
+    for index in range(MAX):
+        print(numbers[index], end='')
+    print()
+
+main()
