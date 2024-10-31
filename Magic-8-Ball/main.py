f0019e0b7e002ccba455ec3896c4b0a0bf2f8af4
HaydinD <haydindavis@crb1.net>
2024-10-31 09:55:22 -0600

    Update main.py

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -1,7 +1,23 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 10/31
 # Magic 8 Ball Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+import random
+
+awnsers=["Yes, of course!",
+        "Without a doubt, yes.",
+        "You can count on it.",
+        "For sure!",
+        "Ask me later.",
+        "I’m not sure.",
+        "I can’t tell you right now.",
+        "I’ll tell you after my nap.",
+        "No way!",
+        "I don’t think so.",
+        "Without a doubt, no.",
+        "The answer is clearly NO."]
+
+question=input("Ask me anything: ")
+print(random.choice(awnsers))
