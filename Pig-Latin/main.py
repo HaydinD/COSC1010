a3bd87112340801a18c9fcf5ea685479ef74a821
HaydinD <haydindavis@crb1.net>
2024-11-07 10:04:29 -0700

    Update main.py

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,15 @@
 #
-# Name
-# Date
+# Haudin Davis  
+# 11/7
 # Pig Latin Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+i= 0
+phrase = input('Enter a phrase: ').split(" ")
+for word in phrase:
+    word = list(word)
+    word.append(word[0] + "AY")
+    word.pop(0)
+    phrase[i] = "".join(word)
+    i+=1
+print(" ".join(phrase))
