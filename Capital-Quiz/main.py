0386c6bbdf5eef7665bab76b47ade511358c9d9f
HaydinD <haydindavis@crb1.net>
2024-11-19 09:48:07 -0700

    Update main.py

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,20 +1,40 @@
 #
-# Name
-# Date
+# Haydin Davis
+# 11/18
 # Capital Quiz Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
-
+NUM_STATES=5
 import random
 
 def main():
+    state_caps=state_cap_dictionary()
+    
+    correct=0
+    incorrect=0
+
+    for count in range(NUM_STATES):
+        state, capital=state_caps.popitem()
+        
+        print('what is the capital of',state, '?',end='')
+        response=input()
+
+        if response.lower()==capital.lower():
+            correct+=1
+            print('Correct!')
+        else:
+            incorrect+=1
+            print('Incorrect.')
+    print('Correct responses: ',correct)
+    print('Incorrect responses: ',incorrect)
     # Initialize dictionary
+def state_cap_dictionary():
     capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
                 'Arizona':'Phoenix', 'Arkansas':'Little Rock',
                 'California':'Sacramento', 'Colorado':'Denver',
                 'Connecticut':'Hartford', 'Delaware':'Dover',
-                'Florida':'Tallahassee', 'Georgia':'Atlanta',
+                 'Florida':'Tallahassee', 'Georgia':'Atlanta',
                 'Hawaii':'Honolulu', 'Idaho':'Boise',
                 'Illinois':'Springfield', 'Indiana':'Indianapolis',
                 'Iowa':'Des Moines', 'Kansas':'Topeka',
@@ -23,7 +43,7 @@ def main():
                 'Massachusetts':'Boston', 'Michigan':'Lansing',
                 'Minnesota':'Saint Paul', 'Mississippi':'Jackson',
                 'Missouri':'Jefferson City', 'Montana':'Helena',
-                'Nebraska':'Lincoln', 'Nevada':'Carson City',
+                 'Nebraska':'Lincoln', 'Nevada':'Carson City',
                 'New Hampshire':'Concord', 'New Jersey':'Trenton',
                 'New Mexico':'Santa Fe', 'New York':'Albany',
                 'North Carolina':'Raleigh', 'North Dakota':'Bismarck',
@@ -35,7 +55,7 @@ def main():
                 'Vermont':'Montpelier', 'Virginia':'Richmond',
                 'Washington':'Olympia', 'West Virginia':'Charleston',
                 'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
-
+    return capitals
     # Local variables
 
     # Continue until user quits the game.
